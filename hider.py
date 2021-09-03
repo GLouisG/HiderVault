315a320e4c755ba745ffbe164ead64ba8702d4f2
GLouisG <louis.gacho@student.moringaschool.com>
2021-09-03 16:01:53 +0300

    create User Class and add constructor for user class

---
+++ hider.py
@@ -0,0 +1,9 @@
+class User:
+  HiderAccs = []
+  '''
+  class that acts as a blueprint for HiderVault accounts
+  '''
+  def __init__(self, hider_name, hider_password):
+    self.hider_name = hider_name
+    self.hider_password = hider_password
+    
